de866395b44e8e240c9e20c3a8738575ac972b6a
Parsha Pourkhomami <parshap+git@gmail.com>
2013-12-05 19:03:07 -0800

    Fix SmartOS issue with terminal when using zsh and tmux

--- recipes/zsh.rb
+++ recipes/zsh.rb
@@ -1,5 +1,15 @@
 HOME = "/home/parshap"
 
+# Fix SmartOS terminal problem - zsh does not find screen-256color def
+if platform?("smartos") then
+  bash "copy screen-256color definition" do
+    code <<-EOH
+      cp /usr/share/lib/terminfo/s/screen-256color \
+        /opt/local/share/lib/terminfo/s/
+    EOH
+  end
+end
+
 git "#{HOME}/.oh-my-zsh" do
   repository 'git://github.com/robbyrussell/oh-my-zsh.git'
   user "parshap"
